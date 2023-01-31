2c2d46ef9c5b8e5221d6fe981a530f1ab5ed19aa
fulgurance <zohran.londais@gmail.com>
2023-01-31 16:49:55 +0000

    Added Ble bash extension installer

---
+++ Ble/0.3.3/0.3.3.cr
@@ -0,0 +1,16 @@
+class Target < ISM::Software
+    
+    def prepareInstallation
+        super
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share")
+        copyDirectory("#{workDirectoryPath(false)}ble-0.3.3","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ble")
+    end
+
+    def showInformations
+        super
+        showInfo("After the installation, if you wish to use Ble, add this to your .bashrc file:")
+        showInfoCode("\t[[ $- == *i* ]] && source ~/.local/share/blesh/ble.sh --attach=none")
+        showInfoCode("\t[[ ${BLE_VERSION-} ]] && ble-attach                                ")
+    end
+
+end
