b32b33780a1b2054c8f9de0a332e6f2b8a510fe2
fulgurance <zohran.londais@gmail.com>
2023-01-31 17:02:13 +0000

    Patched bad version and wrong message

--- Ble/0.3.3/0.3.3.cr
+++ Ble/0.3.3/0.3.3.cr
@@ -3,13 +3,13 @@ class Target < ISM::Software
     def prepareInstallation
         super
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share")
-        copyDirectory("#{workDirectoryPath(false)}ble-0.3.3","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ble")
+        copyDirectory("#{workDirectoryPath(false)}/ble-0.3.3","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ble")
     end
 
     def showInformations
         super
         showInfo("After the installation, if you wish to use Ble, add this to your .bashrc file:")
-        showInfoCode("\t[[ $- == *i* ]] && source ~/.local/share/blesh/ble.sh --attach=none")
+        showInfoCode("\t[[ $- == *i* ]] && source /usr/share/ble/ble.sh --attach=none")
         showInfoCode("\t[[ ${BLE_VERSION-} ]] && ble-attach                                ")
     end
 
