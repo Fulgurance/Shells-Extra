fec224088e444c0c19ede26744aa68fcb301fc30
Fulgurance <zohran.londais@gmail.com>
2024-01-24 19:39:53 +0000

    Updated to new standard

--- Ble/0.3.4/0.3.4.cr
+++ Ble/0.3.4/0.3.4.cr
@@ -10,11 +10,9 @@ class Target < ISM::Software
     def showInformations
         super
 
-        showInfoTitle
         showInfo("After the installation, if you wish to use Ble, add this to your .bashrc file:")
         showInfoCode("\t[[ $- == *i* ]] && source /usr/share/ble/ble.sh --attach=none")
         showInfoCode("\t[[ ${BLE_VERSION-} ]] && ble-attach                          ")
-        showEndInfo
     end
 
 end
